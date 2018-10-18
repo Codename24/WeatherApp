4bf71b7f1b750384d74b399e021730e30965cf57
Markiian Yuskiv <codename24j@gmail.com>
2018-10-18 17:59:39 +0300

    Added Expressions and General Cleanuo

---
+++ Weather App/Weather App/Expressions/WeathedModelsExpressions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using Weather_App.Models;
+using Weather_App.ViewModels;
+
+namespace Weather_App.Expressions
+{
+    public static class WeathedModelsExpressions
+    {
+        public static readonly Expression<Func<CurrentWeatherResponse, WeatherViewModel>>
+            CurrentWeatherResponseToWeatherViewModel = model => new WeatherViewModel
+            {
+                CityName = model.Name,
+                WeatherDetails = new List<WeatherDetailsViewModel>
+                {
+                    new WeatherDetailsViewModel
+                    {
+                        Humidity = model.MainData.Humidity,
+                        Temperature = model.MainData.Temperature,
+                        WindSpeed = model.WindData.Speed,
+                        CalculationDate = DateTime.Now
+                    }
+                }
+            };
+
+        public static readonly Expression<Func<ForecastedWeatherResponse, WeatherViewModel>>
+            ForecastedWeatherResponseToWeatherViewModel =
+                model => new WeatherViewModel
+                {
+                    CityName = model.CityInfo.Name,
+                    WeatherDetails = model.WeatherDataList.Select(c =>
+                        new WeatherDetailsViewModel
+                        {
+                            Humidity = c.MainData.Humidity,
+                            Temperature = c.MainData.Temperature,
+                            WindSpeed = c.WindData.Speed,
+                            CalculationDate = c.CurrentDate
+                        })
+                };
+    }
+}
--- Weather App/Weather App/Services/WeatherService.cs
+++ Weather App/Weather App/Services/WeatherService.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
-using Newtonsoft.Json;
+using Weather_App.Expressions;
 using Weather_App.Interfaces;
 using Weather_App.Models;
 using Weather_App.ViewModels;
@@ -24,7 +20,6 @@ namespace Weather_App.Services
 
         private const string BaseUrlAddres = "http://api.openweathermap.org";
 
-
         public async Task<WeatherViewModel> LoadCurrentWeatherDataForCity(string cityData)
         {
             var zipCode = CheckIfZipCode(cityData);
@@ -33,22 +28,8 @@ namespace Weather_App.Services
                 : $"/data/2.5/weather?q={cityData}&appid={_configuration["OpenWeatherApiKey"]}&units=metric";
             var weatherResult =
                 await _apiInteractionService.GetExternalApiResult<CurrentWeatherResponse>(BaseUrlAddres, paramtetesUrl);
-
-            var result = new WeatherViewModel
-            {
-                CityName = weatherResult.Name,
-                WeatherDetails = new List<WeatherDetailsViewModel>
-                {
-                    new WeatherDetailsViewModel
-                    {
-                        Humidity = weatherResult.MainData.Humidity,
-                        Temperature = weatherResult.MainData.Temperature,
-                        WindSpeed = weatherResult.WindData.Speed,
-                        CalculationDate = DateTime.Now
-                    }
-                }
-            };
-
+            var expression = WeathedModelsExpressions.CurrentWeatherResponseToWeatherViewModel.Compile();
+            var result = expression.Invoke(weatherResult);
             return result;
         }
 
@@ -58,22 +39,11 @@ namespace Weather_App.Services
             var paramtetesUrl = zipCode != 0
                 ? $"/data/2.5/forecast?zip={zipCode}&appid={_configuration["OpenWeatherApiKey"]}&units=metric"
                 : $"/data/2.5/forecast?q={cityData}&appid={_configuration["OpenWeatherApiKey"]}&units=metric";
-
             var rawWeather =
                 await _apiInteractionService.GetExternalApiResult<ForecastedWeatherResponse>(BaseUrlAddres,
                     paramtetesUrl);
-            var result = new WeatherViewModel
-            {
-                CityName = rawWeather.CityInfo.Name,
-                WeatherDetails = rawWeather.WeatherDataList.GroupBy(c => c.CurrentDate.Date).Select(c =>
-                    new WeatherDetailsViewModel
-                    {
-                        Humidity = c.Average(v => v.MainData.Humidity),
-                        Temperature = c.Average(v => v.MainData.Temperature),
-                        WindSpeed = c.Average(v => v.WindData.Speed),
-                        CalculationDate = c.Key.Date
-                    })
-            };
+            var expression = WeathedModelsExpressions.ForecastedWeatherResponseToWeatherViewModel.Compile();
+            var result = expression.Invoke(rawWeather);
             return result;
         }
 
